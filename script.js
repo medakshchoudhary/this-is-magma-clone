ae47b4aad4d9054be21bfeeb177b1ae1305fb517
medakshchoudhary <ch.daksh01@gmail.com>
2023-12-26 19:41:30 +0530

    huh

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-gsap.registerPlugin(ScrollTrigger);
+function loco(){
+  gsap.registerPlugin(ScrollTrigger);
 
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
@@ -37,3 +38,27 @@ ScrollTrigger.scrollerProxy("#main", {
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
 ScrollTrigger.refresh();
+
+}
+loco();
+
+var clutter = "";
+
+document.querySelector("#page2>h1").textContent.split(" ").forEach(function(dets){
+  clutter += `<span> ${dets} </span>`;
+
+  document.querySelector("#page2>h1").innerHTML = clutter;
+})
+
+gsap.to("#page2>h1>span", {
+  scrollTrigger:{
+    trigger:`#page2>h1>span`,
+    start:`top bottom`, // first is for the element value and second is for the screen value 
+    end:`bottom top`,
+    scroller:`#main`,
+    scrub:.5,
+    markers:true,
+  },
+  stagger:.2,
+  color:`white`,
+})
